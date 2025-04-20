d3a5192a235ec29f0dcef9f1d4aab030a2a7f4e1
Indra Nurzaman <nurzamanindra@gmail.com>
2025-04-20 15:28:09 +0700

    add routing get by radius

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Bootcamp = require('../models/Bootcamp');
+const geocoder = require('../utils/geocoder')
 
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
@@ -71,4 +72,36 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   }
 
   res.status(200).json({ success: true, data: {} });
+});
+
+
+// @desc      Get bootcamps within radius
+// @route     GET /api/v1/radius/:zipcode/:distance
+// @access    Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  
+  const {zipcode, distance} = req.params;
+
+  //get latitude/longitude from zipcode
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+console.log(loc)
+
+  //calc earth radius
+  // Divide dist by radius of Earth
+  // Earth Radius = 3,963 mi / 6,378 km
+  const radius = distance / 3963;
+console.log(radius)
+  const bootcamps = await Bootcamp.find(
+    {
+      location: { $geoWithin: { $centerSphere: [ [ lng, lat ], radius ] } }
+    }
+  );
+
+  res.status(200).json({
+    success: true,
+    cont: bootcamps.length,
+    data: bootcamps
+  });
 });
