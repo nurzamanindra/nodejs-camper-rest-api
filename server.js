3902853e523debaf3a3a904aba0548718b048af0
Indra Nurzaman <nurzamanindra@gmail.com>
2025-05-12 21:09:21 +0700

    add users CRUD

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ connectDB();
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
+const users = require('./routes/users');
 const cookieParser = require('cookie-parser');
 
 
@@ -48,6 +49,8 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+
 
 //use error handler
 app.use(errorHandler);
